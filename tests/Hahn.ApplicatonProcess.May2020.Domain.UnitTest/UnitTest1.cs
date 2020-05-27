76e6e129b1ed854e06fcb1d5745f32be1eda8d1b
Hasan <hasan.miah@7peakssoftware.com>
2020-05-27 23:30:49 +0700

    Added Form with Aurelia and Added Unit Test Project

---
+++ tests/Hahn.ApplicatonProcess.May2020.Domain.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Hahn.ApplicatonProcess.May2020.Domain.UnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
